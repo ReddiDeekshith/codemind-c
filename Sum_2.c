dd1c291d8f418de7bdb63cd60b37a36b0151d66b
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2022-12-07 10:22:16 +0530

    #include<stdio.h> int main() {     int i,a,b,x,y,s=0;     scanf("%d%d%d%d",&a,&b,&x,&y);     for(i=a;i<=b;i++)     {         if(i%x==0 && i%y!=0)         {             s=s+i;         }     }     printf("%d",s); }

--- Sum_2.c
+++ Sum_2.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,a,b,c,d,s=0;
-    scanf("%d%d%d%d",&a,&b,&c,&d);
+    int i,a,b,x,y,s=0;
+    scanf("%d%d%d%d",&a,&b,&x,&y);
     for(i=a;i<=b;i++)
     {
-        if(i%c==0 && i%d!=0)
+        if(i%x==0 && i%y!=0)
         {
             s=s+i;
         }
