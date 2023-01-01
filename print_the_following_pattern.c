cc44643ef6f258babee6b4be51079f0c4d808634
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2023-01-01 21:18:54 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<i;j++)         {             printf(" ");         }         for(k=1;k<=n;k++)         {             if(i==n || i==1 || k==1 || k==n )             printf("*");             else             printf(" ");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,i,j,k;
     scanf("%d",&n);
     for(i=n;i>=1;i--)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<i;j++)
         {
-            printf("%c ",i+64);
+            printf(" ");
+        }
+        for(k=1;k<=n;k++)
+        {
+            if(i==n || i==1 || k==1 || k==n )
+            printf("*");
+            else
+            printf(" ");
         }
         printf("
 ");
