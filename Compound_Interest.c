538e59e2f12de112eb4df826883528863b5f4b2d
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2023-01-11 16:07:19 +0530

    #include<stdio.h> #include<math.h> int main() {     double p,r,t,c;     scanf("%lf%lf%lf",&p,&r,&t);     c=p*pow((1+(r/100)),t)-p;     printf("%.2lf",c); }

--- Compound_Interest.c
+++ Compound_Interest.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 #include<math.h>
-float dk(float p,float r,float t)
-{
-    float c;
-    c=p*pow((1+(r/100)),t)-p;
-    return c;
-}
 int main()
 {
-    float p,r,t;
-    scanf("%f%f%f",&p,&r,&t);
-    float x=dk(p,r,t);
-    printf("%.2f",x);
+    double p,r,t,c;
+    scanf("%lf%lf%lf",&p,&r,&t);
+    c=p*pow((1+(r/100)),t)-p;
+    printf("%.2lf",c);
 }
