f5271d0a0ac7d0158bcd1b7fe8f48ac781a21e53
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2023-01-12 14:47:12 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,j,m,max,z=1;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<n;i++)     {         for(j=0;j<n-1;j++)         {             if(a[j]>a[j+1])             {                 m=a[j];                 a[j]=a[j+1];                 a[j+1]=m;             }         }     }     max=a[n-1];     if(n>=3)     {        for(i=n-1;i>=0;i--)         {            if(max>a[i])            {                if(z==3)                {                    break;                }                else                {                    max=a[i];                    z++;                }            }         }         printf("%d",max);     }     else     {         for(i=0;i<n;i++)         {             if(i=n-1)             {                 printf("%d",a[i]);             }         }     } }

---
+++ _Third_Maximum_Number.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j,m,max,z=1;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                m=a[j];
+                a[j]=a[j+1];
+                a[j+1]=m;
+            }
+        }
+    }
+    max=a[n-1];
+    if(n>=3)
+    {
+       for(i=n-1;i>=0;i--)
+        {
+           if(max>a[i])
+           {
+               if(z==3)
+               {
+                   break;
+               }
+               else
+               {
+                   max=a[i];
+                   z++;
+               }
+           }
+        }
+        printf("%d",max);
+    }
+    else
+    {
+        for(i=0;i<n;i++)
+        {
+            if(i=n-1)
+            {
+                printf("%d",a[i]);
+            }
+        }
+    }
+}
